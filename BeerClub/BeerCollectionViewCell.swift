ef6ac9a8447fbf95d27f76987f255713e501b311
Crisis Core <crisiscore.pai93@gmail.com>
2018-11-15 07:48:52 +0630

    Initial commit

---
+++ BeerClub/BeerCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BeerCollectionViewCell.swift
+//  BeerClub
+//
+//  Created by Crisis Core on 11/12/18.
+//  Copyright © 2018 PADC. All rights reserved.
+//
+
+import UIKit
+
+class BeerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
